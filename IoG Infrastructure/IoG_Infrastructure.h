168a684d7368b3c88c47829aab9a2c73b98c9bef
Eric Crichlow <eric@infusionsofgrandeur.com>
2018-11-06 13:25:55 -0700

    Initial Commit

---
+++ IoG Infrastructure/IoG_Infrastructure.h
@@ -0,0 +1,19 @@
+//
+//  IoG_Infrastructure.h
+//  IoG Infrastructure
+//
+//  Created by Eric Crichlow on 11/6/18.
+//  Copyright © 2018 Infusions of Grandeur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IoG_Infrastructure.
+FOUNDATION_EXPORT double IoG_InfrastructureVersionNumber;
+
+//! Project version string for IoG_Infrastructure.
+FOUNDATION_EXPORT const unsigned char IoG_InfrastructureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IoG_Infrastructure/PublicHeader.h>
+
+
